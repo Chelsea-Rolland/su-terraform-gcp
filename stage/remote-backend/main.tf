43e6f05e4fb02c08dfd2643caab77ca292997570
Rolland <chelsea.rolland@accenture.com>
2022-08-26 16:12:06 -0500

    added the remote backend to generate the bucket to store the tfstate files inside of gcp, will look next week to see if there is a way to do this with 1 set of code

---
+++ stage/remote-backend/main.tf
@@ -0,0 +1,19 @@
+provider "google" {
+    project = var.gcp_project_id
+    region  = var.gcp_region
+    zone = var.gcp_zone
+}
+
+resource "random_integer" "randomInt" {
+    min = 1111111
+    max = 9999999
+}
+
+resource "google_storage_bucket" "tf-state" {
+    name          = "${var.app_environment}-${random_integer.randomInt.id}-tfstate"
+    location      = var.bucket_location
+    force_destroy = true
+    versioning {
+        enabled = true
+    }
+}
