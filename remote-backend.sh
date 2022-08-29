7a012df53745199f517fd891f8ff7ea356bdf858
Rolland <chelsea.rolland@accenture.com>
2022-08-29 16:13:52 -0500

    change file name to terraform.tfvars in remote-backend module, added scripts for remote-backend and updated run script and tested it out

---
+++ remote-backend.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+cd modules/remote-backend
+
+terraform fmt
+
+# Backend will be local since its a simple bucket creation
+terraform init
+
+# Makes sure our basic configs are correct. This will not catch everything, but its a good practice to implement for basic config
+terraform validate
+
+terraform plan -var-file="terraform.tfvars" -out="remote-backend.tfplan"
+
+terraform apply "remote-backend.tfplan"
+
+# Informing us it has been completed
+echo "COMPLETED CREATING REMOTE BACKEND BUCKET"
+
+# Returning to home directory
+cd ../..
+
+echo "BACK IN ROOT DIRECTORY"
+
+pwd
--- run.sh
+++ run.sh
@@ -2,32 +2,50 @@
 
 TF_ENV=$1
 
+echo "Inputted Environment = [ $TF_ENV ]"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "$DIR"
+echo "DIR = $DIR"
 
 # Always run from the location of this script
 cd $DIR
 
 if [ $# -gt 0 ]; then
-    if [ "$2" == "init"]; then
+    if [ "$2" == "init" ]; then
         terraform -chdir=./$TF_ENV init
         terraform -chdir=./$TF_ENV workspace list
         ENV_WRKSP=$(terraform -chdir=./$TF_ENV workspace show)
         echo "Current Workspace ENV is [ $ENV_WRKSP ]"
-        if [ $TF_ENV eq $ENV_WRKSP]; then
+        if [ $TF_ENV == $ENV_WRKSP ]; then
             terraform -chdir=./$TF_ENV workspace select $TF_ENV
+            CHG_WKSP=$(terraform -chdir=./$TF_ENV workspace show)
+            echo "Workspace has been changed to [ $CHG_WKSP ]"
         else 
             terraform -chdir=./$TF_ENV workspace new $TF_ENV
             terraform -chdir=./$TF_ENV workspace select $TF_ENV
-            echo "New/Current Workspace ENV is [ $ENV_WRKSP ]"
+            NEW_WKSP=$(terraform -chdir=./$TF_ENV workspace show)
+            echo "New/Current Workspace ENV is [ $NEW_WKSP ]"
         fi
-    else if [ "$2" == "plan"]; then
+    echo "COMPLETED TERRAFORM TASK - [ $2 ]"
+    elif [ "$2" == "plan" ]; then
+        terraform -chdir=./$TF_ENV workspace select $TF_ENV
+        ENV_WRKSP1=$(terraform -chdir=./$TF_ENV workspace show)
+        echo "Current Workspace ENV is [ $ENV_WRKSP1 ]"
         terraform -chdir=./$TF_ENV $2 -out="tf-$TF_ENV.tfplan"
-    else if [ "$2" == "apply"]; then
+        echo "COMPLETED TERRAFORM TASK - [ $2 ]"
+    elif [ "$2" == "apply" ]; then
+        terraform -chdir=./$TF_ENV workspace select $TF_ENV
+        ENV_WRKSP2=$(terraform -chdir=./$TF_ENV workspace show)
+        echo "Current Workspace ENV is [ $ENV_WRKSP2 ]"
         terraform -chdir=./$TF_ENV $2 "tf-$TF_ENV.tfplan"
+        echo "COMPLETED TERRAFORM TASK - [ $2 ]"
     else
+        terraform -chdir=./$TF_ENV workspace select $TF_ENV
+        ENV_WRKSP3=$(terraform -chdir=./$TF_ENV workspace show)
+        echo "Current Workspace ENV is [ $ENV_WRKSP3 ]"
         terraform -chdir=./$TF_ENV $2
+        echo "COMPLETED TERRAFORM TASK - [ $2 ]"
     fi
 fi
 
